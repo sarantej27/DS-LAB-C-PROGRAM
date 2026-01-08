18052b2f8b6288554c1cba4f63a0158612e8500a
sarantej27 <sarantej27@gmail.com>
2026-01-08 14:56:22 +0530

    diamond pattern printing program
    This program prompts the user for the number of rows and prints a half diamond pattern using asterisks.

---
+++ diamond_pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j;
+    
+    printf("Enter number of rows (half diamond): ");
+    scanf("%d", &rows);
+    
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for (i = rows - 1; i >= 1; i--) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+     return 0;
+}
